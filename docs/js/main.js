ae0dad3a38fdbb472c9ec900f7ac0c78b71468ae
Sina Khanifar <sina.khanifar@gmail.com>
2015-03-31 21:50:43 -0700

    js cleanup and rewrite nav show/hide

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,20 @@
-$(document).on('ready', function () {
-    /* ==========================================================================
+$(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 200) {
+            $('.navbar-inverse').fadeIn(500);
+        } else {
+            $('.navbar-inverse').fadeOut(500);
+        }
+    });
+
+
+    $('#video-container').click(function(){
+      $('#video-container').html('<iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/pF7d8Dmxy8A?rel=0&vq=hd1080&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    });
+
+    /* ================================
        Social counts
-       ========================================================================== */
+    ================================= */
 
     var shareUrl = window.location.href;
     // If the share buttons widget is visible, lets load some values
@@ -11,10 +24,10 @@ $(document).on('ready', function () {
                 $.each(res, function(network, value) {
                     var count = value;
                     if (count / 10000 > 1) {
-                        count = Math.ceil(count / 1000) + 'k'
+                        count = Math.ceil(count / 1000) + 'k';
                     }
                     $('[data-network="' + network + '"]').attr('count', count).hide().show();
-                })
+                });
             },
             dataType: 'json',
             cache         : true
@@ -24,33 +37,16 @@ $(document).on('ready', function () {
           var url = $(this).attr("href");
           window.open(url, "Share on Facebook", "width=650,height=500");
           return false;
-        })
+        });
         $( ".twitter-button" ).click(function() {
             var url = $(this).attr("href");
             window.open(url,"Twitter","width=550,height=420");
             return false;
-        })
+        });
         $( ".google-button" ).click(function() {
             var url = $(this).attr("href");
             window.open(url,"Share on Google Plus","width=500,height=436");
             return false;
-        })
-    }
-
-});
-
-$('#video-container').click(function(){
-  $('#video-container').html('<iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/pF7d8Dmxy8A?rel=0&vq=hd1080&autoplay=1" frameborder="0" allowfullscreen></iframe>');
-});
-
-$(window).on('scroll', function() {
-    if ($(this).scrollTop() > 150 && !$('.navbar').hasClass('visible')) {
-        $('.navbar').animate({opacity : 1}, 'slow', function() {
-            $(this).addClass('visible').removeAttr('style');
-        });
-    } else if ($(this).scrollTop() <= 150 && $('.navbar').hasClass('visible')) {
-        $('.navbar').animate({opacity : 0}, 'slow', function() {
-            $(this).removeClass('visible').removeAttr('style');
         });
     }
 });
